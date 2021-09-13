472e9d20b57470ccb15feb07a9d6d6aaa3f5aef1
Tegar Raharditya <tegar.raharditya@gmail.com>
2021-09-13 16:11:54 +0800

    add selector

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -3,16 +3,16 @@
     <form>
       <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
         <label>Quote</label>
-        <textarea id="inputQuote" class="form-control" rows="3" v-model="quote"></textarea>
+        <textarea id="inputQuote" data-testid="textInputQuote" class="form-control" rows="3" v-model="quote"></textarea>
       </div>
       <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
         <label>Color</label>
-        <select id="colorSelect" class="form-control" rows="3" v-model="color">
+        <select id="colorSelect" data-testid="colourSelect" class="form-control" rows="3" v-model="color">
           <option v-for="option in options" :value="option">{{ option }}</option>
         </select>
       </div>
       <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
-        <button id="buttonAddQuote" class="btn btn-primary" @click.prevent="createNew">Add Quote
+        <button id="buttonAddQuote" data-testid="btnSubmitQuote" class="btn btn-primary" @click.prevent="createNew">Add Quote
         </button>
       </div>
     </form>
